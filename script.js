3d734a9d073f3ab77f96e620880b104a79bedcb5
JonathanKonzen <jonathan.conze@gmail.com>
2026-06-02 11:17:06 -0300

    Alteração Cód JS

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-function generateNumber(){
+function generateNumber() {
     const min = Math.ceil(document.querySelector('.input-min').value);
     const max = Math.floor(document.querySelector('.input-max').value);
-    
-    const result = Math.floor(Math.random() * (max - min) + min);
-    
-    alert(`O número sorteado foi: ${result}`);
+
+    if (min > max) {
+        alert('O valor mínimo deve ser menor que o valor máximo.');
+    } else {
+        const result = Math.floor(Math.random() * (max - min) + min);
+        alert(`O número sorteado foi: ${result}`);
+    }
+
+
+
+
+
 
 }
